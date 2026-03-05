13c50775ce9024b88e5c14755ad430c6d2ab48e3
Dayman Novaes <daymannovaes@gmail.com>
2026-03-05 17:01:52 -0300

    coverage

--- vitest.config.js
+++ vitest.config.js
@@ -3,5 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     restoreMocks: true,
+    coverage: {
+      include: ['commands/**', 'lib/**'],
+      reporter: ['text', 'html'],
+    },
   },
 });
